58add31d7458a06942443d3451cc3566d3cf2d9d
ybales-jonelecs <naboraybales10@gmail.com>
2026-08-11 01:12:53 +0800

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAy1hi8vq_bOCpsmCDxgof49cYLaCJ46lg",
+  authDomain: "chats-54e77.firebaseapp.com",
+  projectId: "chats-54e77",
+  storageBucket: "chats-54e77.firebasestorage.app",
+  messagingSenderId: "851919939421",
+  appId: "1:851919939421:web:80650b62201b27d3a4aab4",
+  measurementId: "G-9PM2NYMJVJ"
+};
+
+export const vapidKey = "";
