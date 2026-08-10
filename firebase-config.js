c5087c385264cc90772a8017271d3581e8dacb95
ybales-jonelecs <naboraybales10@gmail.com>
2026-08-11 00:49:28 +0800

    firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,3 @@
-// Firebase configuration for your Messenger app.
 export const firebaseConfig = {
   apiKey: "AIzaSyAy1hi8vq_bOCpsmCDxgof49cYLaCJ46lg",
   authDomain: "chats-54e77.firebaseapp.com",
@@ -10,5 +9,4 @@ export const firebaseConfig = {
   databaseURL: "https://chats-54e77-default-rtdb.firebaseio.com"
 };
 
-// Add your Web Push certificate key here later for notifications.
 export const vapidKey = "YOUR_WEB_PUSH_VAPID_KEY";
