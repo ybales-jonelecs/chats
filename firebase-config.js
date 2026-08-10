289571f45bbc6b49685629bdfef5be49c0ec518f
ybales-jonelecs <naboraybales10@gmail.com>
2026-08-11 00:42:31 +0800

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+// Firebase configuration for your Messenger app.
+export const firebaseConfig = {
+  apiKey: "AIzaSyAy1hi8vq_bOCpsmCDxgof49cYLaCJ46lg",
+  authDomain: "chats-54e77.firebaseapp.com",
+  projectId: "chats-54e77",
+  storageBucket: "chats-54e77.firebasestorage.app",
+  messagingSenderId: "851919939421",
+  appId: "1:851919939421:web:91b03137fde446b7a4aab4",
+  measurementId: "G-EY7ZHB6Y1N",
+  databaseURL: "https://chats-54e77-default-rtdb.firebaseio.com"
+};
+
+// Add your Web Push certificate key here later for notifications.
+export const vapidKey = "YOUR_WEB_PUSH_VAPID_KEY";
